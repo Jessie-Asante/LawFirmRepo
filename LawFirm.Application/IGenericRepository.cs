d44ad414f81e6f686cf89ce783725a014d6b5970
Jessie-Asante <kaprikon1995@gmail.com>
2023-07-19 00:55:07 +0100

    RepoUpdate

--- LawFirm.Application/IGenericRepository.cs
+++ LawFirm.Application/IGenericRepository.cs
@@ -3,6 +3,7 @@
     public interface IGenericRepository<TEntity> where TEntity : class
     {
         Task<TEntity> AddAsync(TEntity entity);
+        Task<bool> DeleteAsync(int id);
         Task<IEnumerable<TEntity>> GetAllAsync();
         Task<TEntity> GetById(int id);
         Task<bool> UpdateAsync(TEntity entity);
