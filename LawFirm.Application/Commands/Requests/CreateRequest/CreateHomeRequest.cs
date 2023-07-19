c6dc1c895f72db7b284f243ee7eeeda70b6792ec
Jessie-Asante <kaprikon1995@gmail.com>
2023-07-19 20:46:16 +0100

    add dto and commands

---
+++ LawFirm.Application/Commands/Requests/CreateRequest/CreateHomeRequest.cs
@@ -0,0 +1,15 @@
+﻿using LawFirm.Application.BaseDtos.CommandDtos;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LawFirm.Application.Commands.Requests.CreateRequest
+{
+    public class CreateHomeRequest : IRequest<int>
+    {
+        public CommandHomesDto create { get; set; }
+    }
+}
