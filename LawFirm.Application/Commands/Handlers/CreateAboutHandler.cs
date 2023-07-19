0eb17be6be554f8a98f4e2bd1375b9a34befd360
Jessie-Asante <kaprikon1995@gmail.com>
2023-07-19 21:20:00 +0100

    add

---
+++ LawFirm.Application/Commands/Handlers/CreateAboutHandler.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using LawFirm.Infrastructure.Persistence;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LawFirm.Application.Commands.Handlers
+{
+    public class CreateAboutHandler:IRequestHandler<CreateAboutRequest, int>
+    {
+        private readonly IGenericRepository<TblAboutTag> _repo;
+        private readonly IMapper _mapper;
+        public CreateAboutHandler(IGenericRepository<TblAboutTag> repo, IMapper mapper)
+        {
+            _repo = repo;
+            _mapper = mapper;
+        }
+    }
+}
--- LawFirm.Application/Commands/Handlers/CreateHomeHandler.cs
+++ LawFirm.Application/Commands/Handlers/CreateHomeHandler.cs
@@ -1,7 +1,30 @@
-﻿namespace LawFirm.Application.Commands.Handlers
-{
-    public class CreateHomeHandler
-    {
+﻿using AutoMapper;
+using LawFirm.Application.Commands.Requests.CreateRequest;
+using LawFirm.Domain.Models;
+using LawFirm.Infrastructure.Persistence;
+using MediatR;
 
+namespace LawFirm.Application.Commands.Handlers
+{
+    public class CreateHomeHandler:IRequestHandler<CreateHomeRequest, int>
+    {
+        private readonly IGenericRepository<TblHomeTag> _repo;
+        private readonly IMapper _mapper;
+
+        public CreateHomeHandler(IGenericRepository<TblHomeTag> repo, IMapper mapper)
+        {
+                _repo = repo;
+                _mapper = mapper;
+        }
+
+        public async Task<int> Handle(CreateHomeRequest request, CancellationToken cancellationToken)
+        {
+            var dto = request.create;
+            var entity = new TblHomeTag();
+            _mapper.Map(dto,entity);
+            FormattableString query = $"[dbo].[spcInsertHomeTag] @Image = {request.create.Image}, @ImageHeader = {request.create.ImageHeader}, @Caption = {request.create.Caption}";
+            var response = await _repo.AddAsync(query);
+            return response;
+        }
     }
 }
